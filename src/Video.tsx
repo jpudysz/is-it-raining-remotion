eccadc3463b09a6ebb1bf91a600aa71ef5916254
jpudysz <jacekpudysz@gmail.com>
2021-12-08 19:26:22 +0100

    Initial commit

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,15 +1,11 @@
-import {Composition} from 'remotion';
-import {HelloWorld} from './HelloWorld';
-import {Logo} from './HelloWorld/Logo';
-import {Subtitle} from './HelloWorld/Subtitle';
-import {Title} from './HelloWorld/Title';
+import {Composition} from 'remotion'
+import { Test } from './components'
 
 export const RemotionVideo: React.FC = () => {
 	return (
-		<>
 			<Composition
 				id="HelloWorld"
-				component={HelloWorld}
+				component={Test}
 				durationInFrames={150}
 				fps={30}
 				width={1920}
@@ -19,34 +15,5 @@ export const RemotionVideo: React.FC = () => {
 					titleColor: 'black',
 				}}
 			/>
-			<Composition
-				id="Logo"
-				component={Logo}
-				durationInFrames={200}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Title"
-				component={Title}
-				durationInFrames={100}
-				fps={30}
-				width={1920}
-				height={1080}
-				defaultProps={{
-					titleText: 'Welcome to Remotion',
-					titleColor: 'black',
-				}}
-			/>
-			<Composition
-				id="Subtitle"
-				component={Subtitle}
-				durationInFrames={100}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-		</>
-	);
-};
+	)
+}
