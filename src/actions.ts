80fc45b93767f8d96d740f700382cefc162625bb
Jacek Pudysz <jacekpudysz@gmail.com>
2021-12-25 22:17:54 +0100

    Update actions.ts

--- src/actions.ts
+++ src/actions.ts
@@ -11,7 +11,7 @@ export const fetchWeatherDataForCity = async (city: string) => {
         .then(response => response.json())
         .then(data => toWeatherModel(data))
         .catch(error => {
-            throw new Error(JSON.stringify(url))
+            throw new Error(url)
         })
 
     return {
